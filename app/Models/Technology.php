c9c60e6dd34d4d14f28f07fc63da2d49c0536ac8
simone-danzeri <s.danzeri@icloud.com>
2024-06-06 13:36:48 +0200

    feat: added pivot table

--- app/Models/Technology.php
+++ app/Models/Technology.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Technology extends Model
 {
     use HasFactory;
+    public function projects() {
+        return $this->belongsToMany(Project::class);
+    }
 }
